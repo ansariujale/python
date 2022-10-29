d3e000b910192efdac18f8abf5a632d49ee57238
ansariujale <116936074+ansariujale@users.noreply.github.com>
2022-10-29 11:19:05 +0545

    Create practical 1.py

---
+++ practical 1.py
@@ -0,0 +1,7 @@
+letter = input("input a letter of an alphabet:")
+if letter in ('a','e','i','o','u'):
+   print("It is a vowel")
+elif letter =='y':
+   print("sometimes letter y stands for vowel, sometimes it stands for consonant")
+else:
+   print("it is a consonant")
